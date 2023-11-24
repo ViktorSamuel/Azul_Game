05ca59d8f8ab73d7e3bfd9b6028fd349d4081fd2
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-11-24 12:16:18 +0100

    Refactor UsedTiles to implement its interface and fix miss casting in give method attr

--- src/main/java/sk/uniba/fmph/dcs/UsedTiles.java
+++ src/main/java/sk/uniba/fmph/dcs/UsedTiles.java
@@ -2,7 +2,7 @@ package sk.uniba.fmph.dcs;
 
 import java.util.*;
 
-public class UsedTiles {
+public class UsedTiles implements UsedTilesGiveInterface {
     private final List<Tile> usedTiles; //List to hold used tiles
 
     public UsedTiles() {
@@ -10,7 +10,8 @@ public class UsedTiles {
     }
 
     // Method to add an array of Tile objects to the usedTiles list without STARTING_PLAYER tile.
-    public void give(Tile[] tiles) {
+    @Override
+    public void give(Collection<Tile> tiles) {
         if (tiles == null) {
             throw new IllegalArgumentException("Cannot add null array of tiles.");
         }
@@ -22,6 +23,18 @@ public class UsedTiles {
             this.usedTiles.add(tile);
         }
     }
+//    public void give(Tile[] tiles) {
+//        if (tiles == null) {
+//            throw new IllegalArgumentException("Cannot add null array of tiles.");
+//        }
+//        for (Tile tile : tiles) {
+//            // Check if the current tile is the STARTING_PLAYER tile
+//            if (tile == Tile.STARTING_PLAYER) {
+//                continue;
+//            }
+//            this.usedTiles.add(tile);
+//        }
+//    }
 
     // Method to retrieve and remove all Tile objects from the usedTiles list.
     public Tile[] takeAll() {
