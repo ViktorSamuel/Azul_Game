1961a70f32db7d219698b1f86a79bc13d29b3eb4
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-11-24 12:15:00 +0100

    Refactoring PatternLine class and testing it

--- src/main/java/sk/uniba/fmph/dcs/PatternLine.java
+++ src/main/java/sk/uniba/fmph/dcs/PatternLine.java
@@ -1,16 +1,16 @@
 package sk.uniba.fmph.dcs;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class PatternLine implements PatternLineInterface{
     private int capacity;
     private ArrayList<Tile> tiles;
-    private Floor floor;
-    private WallLine wallLine;
-    private UsedTiles usedTiles;
-    public PatternLine(int capacity, Floor floor, WallLine wallLine, UsedTiles usedTiles){
+    private FloorInterface floor;
+    private WallLineInterface wallLine;
+    private UsedTilesGiveInterface usedTiles;
+    private Tile color = null;
+    public PatternLine(int capacity, FloorInterface floor, WallLineInterface wallLine, UsedTilesGiveInterface usedTiles){
         this.floor = floor;
         this.wallLine = wallLine;
         this.capacity = capacity;
@@ -18,10 +18,16 @@ public class PatternLine implements PatternLineInterface{
         tiles = new ArrayList<Tile>();
     }
     public void put(Tile[] tiles){
-        boolean canPut = wallLine.canPutTile(tiles[0]);
+        boolean canPut = wallLine.canPutTile(tiles[0]) && (color == null || color == tiles[0]);
         Collection<Tile> redundant = new ArrayList<Tile>();
         for(Tile tile : tiles){
-            if(canPut && this.tiles.size() < capacity) this.tiles.add(tile);
+            if(tile == null) continue;
+            if(canPut && this.tiles.size() < capacity) {
+                if (color == null) {
+                    color = tile;
+                }
+                this.tiles.add(tile);
+            }
             else redundant.add(tile);
         }
         floor.put(redundant);
@@ -30,12 +36,15 @@ public class PatternLine implements PatternLineInterface{
         int points = 0;
         if(tiles.size() == capacity){
             points =  wallLine.putTile(tiles.remove(tiles.size()-1)).getValue();
-            usedTiles.give(tiles.toArray(new Tile[tiles.size()]));
+            usedTiles.give(tiles);
             tiles = new ArrayList<Tile>();
+            color = null;
         }
         return new Points(points);
     }
     public String state(){
-        return "Capacity: " + capacity + " | Current state: " + tiles.size() + " | Color: " + tiles.get(0).toString() + "\n";
+        String state = "EMPTY";
+        if(color != null) state = tiles.get(0).toString();
+        return "Capacity: " + capacity + " | Current state: " + tiles.size() + " | Color: " + state + "\n";
     }
 }
---
+++ src/test/java/sk/uniba/fmph/dcs/PatternLineTest.java
@@ -0,0 +1,70 @@
+package sk.uniba.fmph.dcs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+import java.util.ArrayList;
+
+public class PatternLineTest {
+    private Floor floor;
+    private WallLine wallLine;
+    private UsedTiles usedTiles;
+    @Before
+    public void setUp() {
+        usedTiles = new UsedTiles();
+        floor = new Floor(usedTiles, new ArrayList<>());
+        Tile[] tiles = new Tile[Tile.values().length-1];
+        for(int i = 0; i < tiles.length; i++) {
+            tiles[i] = Tile.values()[i+1];
+        }
+        wallLine = new WallLine(tiles, null, null);
+    }
+
+    @Test
+    public void patternLineComplexTest() {
+        PatternLine patternLine = new PatternLine(5, floor, wallLine, usedTiles);
+
+        // Test empty pattern line
+        assertEquals("Capacity: 5 | Current state: 0 | Color: EMPTY\n", patternLine.state());
+        patternLine.put(new Tile[Tile.values().length-1]);
+        assertEquals("Capacity: 5 | Current state: 0 | Color: EMPTY\n", patternLine.state());
+        assertEquals(0, patternLine.finishRound().getValue());
+
+        // Test pattern line with fewer tiles than capacity
+        patternLine.put(new Tile[]{Tile.RED, Tile.RED});
+        assertEquals("Capacity: 5 | Current state: 2 | Color: R\n", patternLine.state());
+        assertEquals(0, patternLine.finishRound().getValue());
+        assertEquals("", floor.state());
+
+        // Test pattern line with wrong color
+        patternLine.put(new Tile[]{Tile.BLUE, Tile.BLUE});
+        assertEquals("Capacity: 5 | Current state: 2 | Color: R\n", patternLine.state());
+        assertEquals(0, patternLine.finishRound().getValue());
+        assertEquals("BB", floor.state());
+        assertEquals("UsedTiles{count=0, usedTiles=[]}", usedTiles.state());
+
+        // Test pattern line with more tiles than capacity
+        patternLine.put(new Tile[]{Tile.RED, Tile.RED, Tile.RED, Tile.RED, Tile.RED, Tile.RED});
+        assertEquals("Capacity: 5 | Current state: 5 | Color: R\n", patternLine.state());
+        assertEquals("BBRRR", floor.state());
+        assertEquals(1, patternLine.finishRound().getValue());
+        assertEquals("UsedTiles{count=4, usedTiles=[R, R, R, R]}", usedTiles.state());
+
+        // Test put into pattern line color that is already in wall line
+        patternLine.put(new Tile[]{Tile.RED, Tile.RED, Tile.RED});
+        assertEquals("Capacity: 5 | Current state: 0 | Color: EMPTY\n", patternLine.state());
+        assertEquals("BBRRRRRR", floor.state());
+        assertEquals(0, patternLine.finishRound().getValue());
+        assertEquals("UsedTiles{count=4, usedTiles=[R, R, R, R]}", usedTiles.state());
+
+        // Test full pattern line with correct color
+        patternLine.put(new Tile[]{Tile.GREEN, Tile.GREEN, Tile.GREEN, Tile.GREEN, Tile.GREEN});
+        assertEquals("Capacity: 5 | Current state: 5 | Color: G\n", patternLine.state());
+        assertEquals("BBRRRRRR", floor.state());
+        assertEquals(2, patternLine.finishRound().getValue());
+        assertEquals("UsedTiles{count=8, usedTiles=[R, R, R, R, G, G, G, G]}", usedTiles.state());
+    }
+}
