b8ead433b6833e1fa385068dae2328a66eda54a4
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-11-25 12:34:50 +0100

    Implemenatation of integration test for Board and it colaborants

--- src/test/java/sk/uniba/fmph/dcs/BoardIntegrationTest.java
+++ src/test/java/sk/uniba/fmph/dcs/BoardIntegrationTest.java
@@ -1,19 +1,20 @@
 package sk.uniba.fmph.dcs;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class BoardIntegrationTest {
     private BoardInterface board;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         board = GameInitializing.createBoard();
     }
 
     @Test
-    public void testGameIntegration() {
+    public void testGameIntegrationOneHorizontalLine() {
         // Check the initial state of the board
         assertEquals(FinishRoundResult.NORMAL, board.finishRound());
         assertEquals(0, board.getPoints().getValue());
@@ -163,5 +164,62 @@ public class BoardIntegrationTest {
 
         board.endGame();
         assertEquals(11, board.getPoints().getValue());
+    }
+
+    @Test
+    public void testAllEndGameScoringRules() {
+        board = GameInitializing.createBoard();
+        // fill vertical line
+        board.put(0, new Tile[]{Tile.RED});
+        board.put(1, new Tile[]{Tile.BLACK, Tile.BLACK});
+        board.put(2, new Tile[]{Tile.BLUE, Tile.BLUE, Tile.BLUE});
+        board.put(3, new Tile[]{Tile.YELLOW, Tile.YELLOW, Tile.YELLOW, Tile.YELLOW});
+        board.put(4, new Tile[]{Tile.GREEN, Tile.GREEN, Tile.GREEN, Tile.GREEN, Tile.GREEN});
+
+        assertEquals(FinishRoundResult.NORMAL, board.finishRound());
+        assertEquals(15, board.getPoints().getValue());
+        assertEquals("Points [value=15]\n" +
+                "P1: Capacity: 1 | Current state: 0 | Color: EMPTY\n" +
+                "W1: R----\n" +
+                "P2: Capacity: 2 | Current state: 0 | Color: EMPTY\n" +
+                "W2: L----\n" +
+                "P3: Capacity: 3 | Current state: 0 | Color: EMPTY\n" +
+                "W3: B----\n" +
+                "P4: Capacity: 4 | Current state: 0 | Color: EMPTY\n" +
+                "W4: I----\n" +
+                "P5: Capacity: 5 | Current state: 0 | Color: EMPTY\n" +
+                "W5: G----\n" +
+                "Floor: ", board.state());
+
+        // fill diagonal line
+        board.put(1, new Tile[]{Tile.RED, Tile.RED});
+        board.put(2, new Tile[]{Tile.RED, Tile.RED, Tile.RED});
+        board.put(3, new Tile[]{Tile.RED, Tile.RED, Tile.RED, Tile.RED});
+        board.put(4, new Tile[]{Tile.RED, Tile.RED, Tile.RED, Tile.RED, Tile.RED});
+        assertEquals(FinishRoundResult.NORMAL, board.finishRound());
+        assertEquals(20, board.getPoints().getValue());
+
+        // fill horizontal line
+        board.put(0, new Tile[]{Tile.GREEN});
+        assertEquals(FinishRoundResult.NORMAL, board.finishRound());
+        board.put(0, new Tile[]{Tile.YELLOW});
+        assertEquals(FinishRoundResult.NORMAL, board.finishRound());
+        board.put(0, new Tile[]{Tile.BLUE});
+        assertEquals(FinishRoundResult.NORMAL, board.finishRound());
+        board.put(0, new Tile[]{Tile.BLACK});
+        assertEquals(FinishRoundResult.GAME_FINISHED, board.finishRound());
+        assertEquals(36, board.getPoints().getValue());
+        assertEquals("Points [value=36]\n" +
+                "P1: Capacity: 1 | Current state: 0 | Color: EMPTY\n" +
+                "W1: RGIBL\n" +
+                "P2: Capacity: 2 | Current state: 0 | Color: EMPTY\n" +
+                "W2: LR---\n" +
+                "P3: Capacity: 3 | Current state: 0 | Color: EMPTY\n" +
+                "W3: B-R--\n" +
+                "P4: Capacity: 4 | Current state: 0 | Color: EMPTY\n" +
+                "W4: I--R-\n" +
+                "P5: Capacity: 5 | Current state: 0 | Color: EMPTY\n" +
+                "W5: G---R\n" +
+                "Floor: ", board.state());
     }
 }
