5c5d2c17a512b6cc0288693d6315da0975923627
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-11-26 01:02:33 +0100

    feat: Implement Factory class for Azul tile distribution
    - Create Factory class inheriting from TileSource with capacity validation.
    - Implement tile taking logic, ensuring tiles can only be taken once per round.
    - Handle edge cases for tile selection, including STARTING_PLAYER and index bounds.
    - Provide methods to check if factory is empty, start a new round, and get state.
    - Add functionality to fill factory from a Bag and retrieve remaining or available tiles.

---
+++ src/main/java/sk/uniba/fmph/dcs/Factory.java
@@ -0,0 +1,94 @@
+package sk.uniba.fmph.dcs;
+import  java.util.*;
+public class Factory extends TyleSource{
+    private List<Tile> tiles;
+    private final int capacity; // The number of tiles the factory can hold
+    private boolean wasAlreadyTaken = false;
+    //  Create a factory with a given capacity.
+    public Factory(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be greater than 0.");
+        }
+        this.capacity = capacity;
+        this.tiles = new ArrayList<>(capacity);
+    }
+    //  Draw a number idx tiles from the factory.
+    @Override
+    public Tile[] take(int idx) {
+        if (wasAlreadyTaken) {
+            return new Tile[0]; // Return an empty array if the factory was already taken from this round
+        } else {
+            if (idx == Tile.STARTING_PLAYER.ordinal()) {
+                return new Tile[0]; // Return an empty array if the STARTING_PLAYER tile was selected
+            }
+            wasAlreadyTaken = true;
+            if (idx < 0 || idx >= Tile.values().length) {
+                return new Tile[0]; // Return an empty array if the index is out of bounds
+            }
+
+            Tile selectedTile = Tile.values()[idx];
+            List<Tile> takenTiles = new ArrayList<>();
+            List<Tile> remainingTiles = new ArrayList<>();
+
+            // Iterate through the tiles and move the selected tiles to the taken list
+            for (Tile tile : tiles) {
+                if (tile == selectedTile) {
+                    takenTiles.add(tile);
+                } else {
+                    remainingTiles.add(tile);
+                }
+            }
+
+            // Replace the current tiles with the remaining tiles
+            tiles = remainingTiles;
+            return takenTiles.toArray(new Tile[0]); // Return the taken tiles
+        }
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return tiles.isEmpty();
+    }
+
+    @Override
+    public void startNewRound() {
+        tiles.clear();
+        wasAlreadyTaken = false;
+    }
+
+    @Override
+    public String state() {
+        StringBuilder stateBuilder = new StringBuilder("Factory state: ");
+        for (Tile tile : tiles) {
+            stateBuilder.append(tile.toString()).append(" ");
+        }
+        return stateBuilder.toString().trim();
+    }
+    //  Fill the factory with tiles from the bag.
+    public void fillFactory(Bag bag) {
+        while (tiles.size() < capacity && !bag.isEmpty()) {
+            Collections.addAll(tiles, bag.take(1)); // Assume that we take one tile at a time
+        }
+    }
+
+    public List<Tile> getRemainingTiles() {
+        if (wasAlreadyTaken) {
+            return new ArrayList<>(tiles);
+        } else {
+            return new ArrayList<>(); // Return an empty list if the factory was not taken from this round
+        }
+    }
+
+    public List<Tile> getAvailableTiles() {
+        if (wasAlreadyTaken) {
+            return new ArrayList<>(); // Return an empty list if the factory was already taken from this round
+        } else {
+            return new ArrayList<>(tiles); // Return a copy of the tiles
+        }
+    }
+
+    public boolean wasAlreadyTaken() {
+        return wasAlreadyTaken;
+    }
+}
