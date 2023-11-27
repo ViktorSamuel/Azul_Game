bc441eb41c5c29bce0b3adbb7da724589bb3bb38
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-11-27 18:35:24 +0100

    Refactor PatternLine to handle tile placement and round completion logic
    - Add constructor for PatternLine with parameters for capacity, usedTiles, wallLine, and floor.
    - Implement put method to handle adding tiles to the pattern line, including validation against the wall line and floor.
    - Introduce logic to manage the colorOfPatternLine state, ensuring it is set only when the wall line can accept the tile color.
    - Add finishRound method to handle the end of round logic, transferring tiles to the wall line and scoring points.
    - Include state method to return a string representation of the current pattern tiles.
    - Provide getters for capacity and patternTiles.

---
+++ src/main/java/sk/uniba/fmph/dcs/PatternLine.java
@@ -0,0 +1,94 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.*;
+
+public class PatternLine {
+    private int capacity;
+    private int capacityMax;
+    private Tile colorOfPatternLine = null;
+    private List<Tile> patternTiles;
+    private WallLine wallLine;
+    private UsedTiles usedTiles;
+    private Floor floor;
+
+    public PatternLine(int capacity, UsedTiles usedTiles, WallLine wallLine, Floor floor){
+        this.capacityMax = capacity;
+        this.capacity = capacity;
+        patternTiles = new ArrayList<>();
+        this.usedTiles = usedTiles;
+        this.wallLine = wallLine;
+        this.floor = floor;
+    }
+
+    public void put(Tile[] tiles) {
+        if (tiles == null) {
+            throw new IllegalArgumentException("Cannot add null array of tiles.");
+        }
+
+        // Initialize a temporary list to hold tiles that can't be added to the pattern line.
+        List<Tile> toRemoveTiles = new ArrayList<>();
+
+        for (Tile tile : tiles) {
+            if (tile == Tile.STARTING_PLAYER) {
+                usedTiles.give(List.of(tile));
+                continue;
+            }
+
+            if (colorOfPatternLine == null) {
+                // Here you should also check if the wallLine can accept this color.
+                if (wallLine.canPutTile(tile)) {
+                    colorOfPatternLine = tile;
+                } else {
+                    toRemoveTiles.add(tile);
+                    continue;
+                }
+            }
+
+            // Check if the tile color matches and there's capacity.
+            if (tile.ordinal() == colorOfPatternLine.ordinal() && capacity > 0) {
+                patternTiles.add(tile);
+                capacity--;
+            } else {
+                toRemoveTiles.add(tile);
+            }
+        }
+
+        if (!toRemoveTiles.isEmpty()) {
+            floor.put(toRemoveTiles);
+        }
+    }
+
+
+    public Points finishRound(){
+        if (patternTiles.isEmpty()) {
+            return new Points(0);
+        }
+        if (capacity == 0) {
+            Points points = wallLine.putTile(patternTiles.get(0));
+            this.usedTiles.give(patternTiles);
+            patternTiles.clear();
+            colorOfPatternLine = null;
+            capacity = capacityMax;
+            return points;
+        }
+        return new Points(0);
+    }
+
+    public String state(){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Tile tile :
+                patternTiles) {
+            stringBuilder.append(tile.toString());
+        }
+        return stringBuilder.toString();
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public List<Tile> getPatternTiles(){
+        return patternTiles;
+    }
+
+}
