fd16e3914b78957d4d94beeb4a495374739c2fd9
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-11-29 15:22:40 +0100

    add fill, fillWithoutShuffle and isEmpty methods

--- src/main/java/sk/uniba/fmph/dcs/BagInterface.java
+++ src/main/java/sk/uniba/fmph/dcs/BagInterface.java
@@ -1,6 +1,9 @@
 package sk.uniba.fmph.dcs;
-
+import java.util.List;
 public interface BagInterface {
     public Tile[]take(int count);
     public String state();
+    public void fill(List<Tile> newTiles);
+    public void fillWithoutShuffle(List<Tile> newTiles);
+    public boolean isEmpty();
 }
