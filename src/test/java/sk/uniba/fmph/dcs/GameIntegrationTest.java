071b52b9f4868adacefc346c1b8616598c245b60
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-11-29 23:58:52 +0100

    Add integration test for playing a complete game
    This commit adds an integration test for playing a complete game in the Game class. The test covers various game actions, such as player moves, tile placements, and checking the game state after each round. It also checks for the end of the game.
    
    The test ensures that the game is initialized correctly with two boards, a table area, and a bag of tiles. It simulates multiple rounds of gameplay and verifies the game state and player actions throughout the game.
    
    This test serves as a comprehensive validation of the Game class and its interactions with other game components.

--- src/test/java/sk/uniba/fmph/dcs/GameIntegrationTest.java
+++ src/test/java/sk/uniba/fmph/dcs/GameIntegrationTest.java
@@ -62,8 +62,138 @@ public class GameIntegrationTest {
         assertEquals(game.getGameObserver(), gameObserver);
 
         //Test
-        System.out.println(game.take(0, 1, 1, 0));
-        System.out.println(game.getState());
-        System.out.println(bag.state());
+        assertTrue(game.take(0, 1, 2, 0)); table.addTilesToCenter(); //BLUE
+        assertTrue(game.take(1, 2, 1, 0)); table.addTilesToCenter(); //RED
+        assertTrue(game.take(0, 4, 3, 1)); table.addTilesToCenter(); //YELLOW
+        assertTrue(game.take(1, 3, 5, 1)); table.addTilesToCenter(); //GREEN
+        assertTrue(game.take(0, 5, 4, 2)); table.addTilesToCenter(); //BLACK
+
+        assertEquals("Table area:\n" +
+        "TableCenter state: SLBILBIGRLGRBIGR\n" +
+        "Factory state:\n" +
+        "Factory state:\n" +
+        "Factory state:\n" +
+        "Factory state:\n" +
+        "Factory state:\n" +
+        "\n" +
+        "Players:\n" +
+        "Points [value=0]\n" +
+        "P1: Capacity: 0 | Current state: 1 | Color: G\n" +
+        "W1: -----\n" +
+        "P2: Capacity: 1 | Current state: 1 | Color: I\n" +
+        "W2: -----\n" +
+        "P3: Capacity: 2 | Current state: 1 | Color: B\n" +
+        "W3: -----\n" +
+        "P4: Capacity: 4 | Current state: 0 | Color: EMPTY\n" +
+        "W4: -----\n" +
+        "P5: Capacity: 5 | Current state: 0 | Color: EMPTY\n" +
+        "W5: -----\n" +
+        "Floor -> \n" +
+        "Points [value=0]\n" +
+        "P1: Capacity: 0 | Current state: 1 | Color: R\n" +
+        "W1: -----\n" +
+        "P2: Capacity: 1 | Current state: 1 | Color: L\n" +
+        "W2: -----\n" +
+        "P3: Capacity: 3 | Current state: 0 | Color: EMPTY\n" +
+        "W3: -----\n" +
+        "P4: Capacity: 4 | Current state: 0 | Color: EMPTY\n" +
+        "W4: -----\n" +
+        "P5: Capacity: 5 | Current state: 0 | Color: EMPTY\n" +
+        "W5: -----\n" +
+        "Floor -> \n", game.getState());
+
+        assertTrue(game.take(1, 0, 4, 2)); //BLUE
+        assertTrue(game.take(0, 0, 3, 1)); //YELLOW
+        assertTrue(game.take(1, 0, 1, 3)); //RED
+        assertTrue(game.take(0, 0, 2, 4)); //GREEN
+        assertTrue(game.take(1, 0, 5, 4)); //BLACK
+
+        assertEquals("Table area:\n" +
+                "TableCenter state: S\n" +
+                "Factory state: L B I G\n" +
+                "Factory state: R L B I\n" +
+                "Factory state: G R L B\n" +
+                "Factory state: I G R L\n" +
+                "Factory state: B I G R\n" +
+                "\n" +
+                "Players:\n" +
+                "Points [value=1]\n" +
+                "P1: Capacity: 1 | Current state: 0 | Color: EMPTY\n" +
+                "W1: -G---\n" +
+                "P2: Capacity: 2 | Current state: 0 | Color: EMPTY\n" +
+                "W2: -I---\n" +
+                "P3: Capacity: 2 | Current state: 1 | Color: B\n" +
+                "W3: -----\n" +
+                "P4: Capacity: 4 | Current state: 0 | Color: EMPTY\n" +
+                "W4: -----\n" +
+                "P5: Capacity: 2 | Current state: 3 | Color: G\n" +
+                "W5: -----\n" +
+                "Floor -> \n" +
+                "Points [value=2]\n" +
+                "P1: Capacity: 1 | Current state: 0 | Color: EMPTY\n" +
+                "W1: R----\n" +
+                "P2: Capacity: 1 | Current state: 1 | Color: L\n" +
+                "W2: -----\n" +
+                "P3: Capacity: 3 | Current state: 0 | Color: EMPTY\n" +
+                "W3: -B---\n" +
+                "P4: Capacity: 1 | Current state: 3 | Color: R\n" +
+                "W4: -----\n" +
+                "P5: Capacity: 2 | Current state: 3 | Color: L\n" +
+                "W5: -----\n" +
+                "Floor -> \n", game.getState());
+        //Round 2
+        assertTrue(game.take(0, 1, 5, 0)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(1, 2, 5, 0)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(0, 3, 3, 1)); table.addTilesToCenter(); //YELLOW
+        assertTrue(game.take(1, 4, 5, 1)); table.addTilesToCenter(); //GREEN
+        assertTrue(game.take(0, 5, 4, 4)); table.addTilesToCenter(); //BLACK
+
+        assertTrue(game.take(1, 0, 4, 2));
+        assertTrue(game.take(0, 0, 1, 1));
+        assertTrue(game.take(1, 0, 3, 3));
+        assertTrue(game.take(0, 0, 2, 4));
+        assertTrue(game.take(1, 0, 5, 4));
+        //Round 3
+        assertTrue(game.take(0, 1, 3, 0)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(1, 2, 3, 0)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(0, 3, 1, 1)); table.addTilesToCenter(); //YELLOW
+        assertTrue(game.take(1, 4, 1, 1)); table.addTilesToCenter(); //GREEN
+        assertTrue(game.take(0, 5, 4, 4)); table.addTilesToCenter(); //BLACK
+
+        assertTrue(game.take(1, 0, 4, 2));
+        assertTrue(game.take(0, 0, 1, 1));
+        assertTrue(game.take(1, 0, 3, 3));
+        assertTrue(game.take(0, 0, 2, 4));
+        assertTrue(game.take(1, 0, 5, 4));
+        //Round 4
+        assertTrue(game.take(0, 1, 4, 0)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(1, 2, 4, 0)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(0, 3, 3, 1)); table.addTilesToCenter(); //YELLOW
+        assertTrue(game.take(1, 4, 5, 1)); table.addTilesToCenter(); //GREEN
+        assertTrue(game.take(0, 5, 4, 4)); table.addTilesToCenter(); //BLACK
+
+        assertTrue(game.take(1, 0, 4, 2));
+        assertTrue(game.take(0, 0, 1, 1));
+        assertTrue(game.take(1, 0, 3, 3));
+        assertTrue(game.take(0, 0, 2, 4));
+        assertTrue(game.take(1, 0, 5, 4));
+
+        //Round 5
+        assertTrue(game.take(0, 1, 5, 1)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(1, 2, 5, 1)); table.addTilesToCenter(); //BLACK
+        assertTrue(game.take(0, 3, 3, 1)); table.addTilesToCenter(); //YELLOW
+        assertTrue(game.take(1, 4, 5, 1)); table.addTilesToCenter(); //GREEN
+        assertTrue(game.take(0, 5, 4, 4)); table.addTilesToCenter(); //BLACK
+
+        assertTrue(game.take(1, 0, 2, 0));
+        assertTrue(game.take(0, 0, 1, 0));
+        assertTrue(game.take(1, 0, 3, 3));
+        assertTrue(game.take(0, 0, 4, 4));
+        assertTrue(game.take(1, 0, 5, 4));
+
+        //Check if end of game
+        assertEquals(FinishRoundResult.GAME_FINISHED, boards.get(0).finishRound());
+        assertEquals(FinishRoundResult.GAME_FINISHED, boards.get(1).finishRound());
     }
+
 }
